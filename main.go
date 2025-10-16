924a35052b3ca7181b744721b9c9b8a8943f7923
Vitor E. N. de Tulio <86317723+Madruu@users.noreply.github.com>
2025-10-16 16:29:20 -0300

    blablabla

--- main.go
+++ main.go
@@ -1,80 +1,82 @@
-package main;
+package main
 
-import(
+import (
 	"fmt"
-	"github.com/gorilla/websocket"
 	"net/http"
 	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
-//Upgrades HTTP connections to websocket connections
+// Upgrades HTTP connections to websocket connections
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		origin := r.Header.Get("Origin")
-		return origin == "<http://localhost:8080>";
+		return true;
+		// origin := r.Header.Get("Origin")
+		// // Aceita conexões de http://localhost:8080
+		// return origin == "http://localhost:8080"
 	},
 }
 
+// Connecting clients, create broadcast channel and protect clients map
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan []byte)
+var mutex = &sync.Mutex{}
 
-//Connecting clients, create broadcast channel and protect clients map
-var clients = make(map[*websocket.Conn]bool);
-var broadcast = make(chan []byte);
-var mutex = &sync.Mutex{};
-
-func wsHandler(w  http.ResponseWriter, r *http.Request) {
+func wsHandler(w http.ResponseWriter, r *http.Request) {
 	//Opening connection and upgrading connections
 	conn, err := upgrader.Upgrade(w, r, nil)
 
 	if err != nil {
-		fmt.Println("Error upgrading:", err);
-		return;
+		fmt.Println("Error upgrading:", err)
+		return
 	}
 
-	defer conn.Close(); //Close right at end of function
+	defer conn.Close() //Close right at end of function
 
-	mutex.Lock();
-	clients[conn] = true;
-	mutex.Unlock();
+	mutex.Lock()
+	clients[conn] = true
+	mutex.Unlock()
 
 	for {
-		_, message, err := conn.ReadMessage();
+		_, message, err := conn.ReadMessage()
 		if err != nil {
-			mutex.Lock(); //Locks the mutex for secutity
-			delete(clients, conn); //Deletes information in case of emergency
-			mutex.Unlock(); //Unlocks mutex
-			break;
+			mutex.Lock()          //Locks the mutex for secutity
+			delete(clients, conn) //Deletes information in case of emergency
+			mutex.Unlock()        //Unlocks mutex
+			break
 		}
 
-		broadcast <- message;
+		broadcast <- message
 	}
-	
+
 }
 
 func handleMessages() {
 	for {
 		//Grabs next message from broadcast channel
-		message := <-broadcast;
+		message := <-broadcast
 		//Sends message to all clients connected
-		mutex.Lock();
+		mutex.Lock()
 		for client := range clients {
-			err := client.WriteMessage(websocket.TextMessage, message);
-			if err != nil{
-				client.Close();
-				delete(clients, client);
+			err := client.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				client.Close()
+				delete(clients, client)
 			}
 		}
-		mutex.Unlock();
+		mutex.Unlock()
 	}
 }
 
-func main(){
+func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.HandleFunc("/ws", wsHandler);
-	fmt.Println("WebSocket server started on: 8080");
+	http.HandleFunc("/ws", wsHandler)
+	fmt.Println("WebSocket server started on: 8080")
 
-	err := http.ListenAndServe(":8080", nil);
+	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
-		fmt.Println("Error starting server:", err);
+		fmt.Println("Error starting server:", err)
 	}
 }
